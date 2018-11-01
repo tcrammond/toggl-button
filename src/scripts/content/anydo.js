23e488ad1f6bf60765d56ba4d3f5da3339f246df
Maciek Ryłko <rylko.maciej@gmail.com>
2018-11-01 09:54:21 +0100

    Update Any.do integration
    close #1166

--- src/scripts/content/anydo.js
+++ src/scripts/content/anydo.js
@@ -1,5 +1,43 @@
 'use strict';
 
+// any.do Q4 2018
+togglbutton.render('.CardScrollView:not(.toggl)', { observe: true }, elem => {
+  const descriptionElem = $('textarea', elem);
+
+  // since task popup has same selector as overview
+  // do nothing when description is not present
+  if (!descriptionElem) { return; }
+
+  const projectElem = $('.TasksToolBarCategoryTitle');
+
+  const link = togglbutton.createTimerLink({
+    buttonType: 'minimal',
+    className: 'anydo--2018',
+    description: descriptionElem.value,
+    projectName: projectElem ? projectElem.textContent : ''
+  });
+
+  descriptionElem.before(link);
+});
+
+// any.do Q4 2018 tasks lists
+togglbutton.render('.TaskListRow:not(.toggl)', { observe: true }, elem => {
+  const descriptionElem = $('input', elem);
+
+  const projectElem = $('.TasksToolBarCategoryTitle');
+
+  const link = togglbutton.createTimerLink({
+    buttonType: 'minimal',
+    className: 'anydo--2018__taskItem',
+    description: descriptionElem ? descriptionElem.value : '',
+    projectName: projectElem ? projectElem.textContent : ''
+  });
+
+  const container = $('.TaskItem', elem);
+  container.appendChild(link);
+});
+
+// TODO: probably safe to delete below
 togglbutton.render('.dialog:not(.toggl)', { observe: true }, function(elem) {
   var link,
     wrap = createTag('div'),
